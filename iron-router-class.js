791ccf333883047404eac60f540ae5e7eab7e5e4
Joe Burdick <josephdburdick@gmail.com>
2014-12-07 22:09:52 -0500

    Add dynamic paths and multiple routes.

--- iron-router-class.js
+++ iron-router-class.js
@@ -1,14 +1,20 @@
 Router.route('/', function () {
-  var isLoggedin = Session.get('isLoggedIn');
-  
-  this.layout('AppLayout');
-
-  if (isLoggedin){
-    this.render('Home');
-  } else {
-    this.render('Login');
-  }
+  this.layout('Layout');
+  this.render('Blog');
 });
+
+Router.route('/blog/new', function(){
+  this.layout('Layout');
+  this.render('ArticleNew');
+});
+
+Router.route('/blog/:_id', function() {
+  console.log('id: ' + this.params._id);
+  console.dir(this.params);
+  this.layout('Layout');
+  this.render('Article');
+});
+
 
 if (Meteor.isClient) {
   

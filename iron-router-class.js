3c3d3627102bf2ef8783fd445062ac3445c2a851
Joe Burdick <josephdburdick@gmail.com>
2014-12-07 21:37:27 -0500

    Render particular views using .render and how to dynamically change the layout depending on the route.

--- iron-router-class.js
+++ iron-router-class.js
@@ -1,4 +1,14 @@
-Router.route('/', 'Home');
+Router.route('/', function () {
+  var isLoggedin = Session.get('isLoggedIn');
+  
+  this.layout('AppLayout');
+
+  if (isLoggedin){
+    this.render('Home');
+  } else {
+    this.render('Login');
+  }
+});
 
 if (Meteor.isClient) {
   
